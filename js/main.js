74222728434f0d7b0903cd8d64e9ac79bf08dcd0
fb2087 <93234754+fb2087@users.noreply.github.com>
2022-01-24 11:43:59 +0900

    1111

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
-$(function(){
-    
+function prev() {
+    $('.slide li:last').prependTo('.slide_img');
+    $('.slide').css('margin-left', -1200); // $('.slide').css({marginLeft:-1000});
+    $('.slide').stop().animate({ marginLeft: 0 }, 800);
+}
+
+
+
+function next() {
+    $('.slide').stop().animate({ marginLeft: -1200 }, function () {
+        $('.slide li:first').appendTo('.slide_img');
+        $('.slide').css({ marginLeft: 0 });
+    });
+}
+
+
+function slide() {
+    $('.slide').stop().animate({ marginLeft: -1200 }, function () {
+        $('.slide li:first').appendTo('.slide_img');
+        $('.slide').css({ marginLeft: 0 });
+    });
+}
+
+
+
+setInterval(slide, 3000);
+$('.left').click(function () {
+    prev();
+});
+$('.right').click(function () {
+    next();
 });
